af62958754b74eb94e285eca6a2dab60c9d0f869
Makoto Yaguchi <my48568@gmail.com>
2017-11-19 15:43:16 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yaguchi/makoto/aisatsuapp/MainActivity.java
@@ -0,0 +1,54 @@
+package jp.techacademy.yaguchi.makoto.aisatsuapp;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            showTimePickerDialog();
+        }
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        int Aisatsutime = hourOfDay * 60 + minute;
+
+                        if (Aisatsutime >= 120 && Aisatsutime <= 599) {
+                            mTextView.setText("おはよう");
+                        } else if (Aisatsutime >= 600 && Aisatsutime <= 1079) {
+                            mTextView.setText("こんにちは");
+                        } else {
+                            mTextView.setText("こんばんは");
+                        }
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+}
